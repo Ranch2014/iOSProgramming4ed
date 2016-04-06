1679a02e5c8002e53eaa23f9fe8a5b1ba2839c6c
Ranch2014 <Ranch2014@outlook.com>
2016-04-06 20:14:55 +0800

    First commit.

---
+++ Hypnosister/Hypnosister/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  Hypnosister
+//
+//  Created by 焦相如 on 4/3/16.
+//  Copyright © 2016 jaxer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
