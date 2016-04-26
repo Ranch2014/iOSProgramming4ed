f40e38749ce46c687187ced57cf9d7899fcadc49
Ranch2014 <Ranch2014@outlook.com>
2016-04-26 21:04:55 +0800

    First commit

---
+++ 03-ManagingMemoryWithARC/RandomItems/RandomItems/main.m
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by 焦相如 on 3/23/16.
+//  Copyright © 2016 jaxer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // 快速枚举 (若需要在循环体中添加或删除对象，不能使用)
+        for (NSString *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        
+        BNRItem *backpack = [[BNRItem alloc] initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        
+        BNRItem *calculator = [[BNRItem alloc] initWithItemName:@"Calculator"];
+        [items addObject:calculator];
+        
+        backpack.containedItem = calculator;
+        backpack = nil;
+        calculator = nil;
+        
+        NSLog(@"Setting items to nil ...");
+        
+        // 释放 items 所指向的 NSMutableArray 对象
+        items = nil;
+    }
+    return 0;
+}
