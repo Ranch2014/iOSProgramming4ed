2677db51b2cdf8e36c52703178be16c73418da78
Ranch2014 <Ranch2014@outlook.com>
2016-05-09 10:58:46 +0800

    first commit
    添加第11章

---
+++ 11-Camera/Homepwner/Homepwner/BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by 焦相如 on 5/6/16.
+//  Copyright © 2016 jaxer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
